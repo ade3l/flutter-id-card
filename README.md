a920d855ce5af568c879e8820a191acab4c69160
Adeel <71775487+ade3l@users.noreply.github.com>
2021-10-08 07:50:04 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# id_card
+
+Flutter app to show an id card. Made to learn the basics of flutter.
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home:idCard()
+  ));
+}
+
+class idCard extends StatelessWidget {
+  const idCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
